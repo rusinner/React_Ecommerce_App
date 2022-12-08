a93e4eae5eb396aeb6606f46df84026571288bbf
Theologos Mavropoulos <rusinner88@gmail.com>
2022-12-08 15:56:41 +0200

    connect to sanity

--- next-ecommerce/pages/index.js
+++ next-ecommerce/pages/index.js
@@ -5,7 +5,7 @@ import { Product, FooterBanner, Herobanner } from "../components";
 const Home = () => {
   return (
     <>
-      <p>Herobanner</p>
+      <Herobanner />
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
@@ -14,7 +14,7 @@ const Home = () => {
         {["Product 1", " Product 2"].map((product) => product)}
       </div>
 
-      <p>Footer</p>
+      <FooterBanner />
     </>
   );
 };
