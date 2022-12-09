49494ec2c6e4e59b42dfe8d6e0b609dbf10fa0db
Theologos Mavropoulos <rusinner88@gmail.com>
2022-12-09 02:15:23 +0200

    Navbar

--- next-ecommerce/components/Layout.jsx
+++ next-ecommerce/components/Layout.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
-const Layout = () => {
-  return <div></div>;
-};
+import Head from "next/head";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
+const Layout = ({ children }) => {
+  return (
+    <div className="layout">
+      <Head>
+        <title>Tech Web Store</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">{children}</main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
+};
 export default Layout;
