07e458dac721bc06c976715e235a4af0ce243fd6
Theologos Mavropoulos <rusinner88@gmail.com>
2022-12-10 14:11:48 +0200

    cart buy now button with stripe

--- next-ecommerce/pages/api/stripe.js
+++ next-ecommerce/pages/api/stripe.js
@@ -3,8 +3,16 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 export default async function handler(req, res) {
   if (req.method === "POST") {
     try {
-      // Create Checkout Sessions from body params.
-      const session = await stripe.checkout.sessions.create({
+      const params = {
+        //custom props i inserted
+        submit_type: "pay",
+        mode: "payment",
+        payment_method_types: ["card"],
+        billling_adress_collection: "auto",
+        shipping_options: [
+          { shipping_rate: "shr_1MDRrCDcgxGsGNrZvTiq8n61" },
+          { shipping_rate: "shr_1MDRseDcgxGsGNrZJxfTwIV3" },
+        ],
         line_items: [
           {
             // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
@@ -15,7 +23,9 @@ export default async function handler(req, res) {
         mode: "payment",
         success_url: `${req.headers.origin}/?success=true`,
         cancel_url: `${req.headers.origin}/?canceled=true`,
-      });
+      };
+      // Create Checkout Sessions from body params.
+      const session = await stripe.checkout.sessions.create(params);
       res.redirect(303, session.url);
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message);
