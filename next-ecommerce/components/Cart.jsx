0a674bfdd745d40beaca30966be4fe628b13ac36
Theologos Mavropoulos <rusinner88@gmail.com>
2022-12-10 12:35:31 +0200

    price to fixed

--- next-ecommerce/components/Cart.jsx
+++ next-ecommerce/components/Cart.jsx
@@ -99,7 +99,7 @@ const Cart = () => {
           <div className="cart-bottom">
             <div className="totl">
               <h3>Subtotal</h3>
-              <h3>{totalPrice} €</h3>
+              <h3>{totalPrice.toFixed(2)} €</h3>
             </div>
             <div className="btn-container">
               <button type="button" className="btn" onClick="">
