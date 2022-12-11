4583df9c55d5266db17ed58f6cc7c2fd752bd2d8
Theologos Mavropoulos <rusinner88@gmail.com>
2022-12-11 15:32:13 +0200

    success.js creation

---
+++ next-ecommerce/pages/success.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import { BsBagCheckFill } from "react-icons/bs";
+import { useRouter } from "next/router";
+
+import { useStateContext } from "../context/StateContext";
+
+const Success = () => {
+  return <div>success</div>;
+};
+
+export default Success;
